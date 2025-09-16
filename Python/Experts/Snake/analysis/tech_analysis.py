152f25ea9e085f45e5369e54f06920ee89a67bbb
vikneo <vm-viktor@mail.ru>
2025-09-16 14:44:46 +0700

    update (add power trend) tech_analysis.py

--- Python/Experts/Snake/analysis/tech_analysis.py
+++ Python/Experts/Snake/analysis/tech_analysis.py
@@ -1,5 +1,7 @@
 import datetime
 import json
+import os
+from pathlib import Path
 from time import sleep
 from typing import Dict
 
@@ -7,94 +9,124 @@ import requests
 from bs4 import BeautifulSoup, ResultSet
 from requests import Response
 
-
-def get_analysis(currency_pair: str) -> Dict[str, str]:
-    """
-    Парсит страницу "investing.com" и собирает данные по отдельным валютным парам.
-
-    :param currency_pair: str - url c валютной парой;
-    :return: Dict[str, str] - словарь с детальной информацией по валютной паре;
-    """
-
-    context: dict = {}
-    try:
-        page: Response = requests.get(currency_pair)
-        soup: BeautifulSoup = BeautifulSoup(page.text, "html.parser")
-
-        title_pair = soup.find("a", attrs={"id": "quoteLink"})
-        data_vol: ResultSet = soup.find_all("span", attrs={"class": "uppercaseText"})
-        indicators: ResultSet = soup.find_all("p", attrs={"class": "inlineblock"})
-        data_now = datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")
-
-        context.update(title=title_pair["title"], date_time=data_now)  # type: ignore
-
-        for name in data_vol:
-            context.update(resume=name.text)
-
-        context.update(results=details(indicators))
-    except OSError as e:
-        context.update(
-            error=type(e),
-            error_message=str(e),
-        )
-
-    save_file(context=context)
-    return context
+BASE_DIR = Path(__file__).parent
+DATA_DIR = BASE_DIR / "data_json"
+DATA_DIR.mkdir(exist_ok=True)
 
 
-def details(data: ResultSet) -> Dict[str, str]:
-    """
-    Собираем детальную информацию по уровням сигналов индикаторов
-    и возвращаем данные в виде словаря.
+class PowerTrend:
 
-    :param data: ResultSet - данные индикаторов;
-    :return: Dict[str, str] - упакованный словарь с детальной информацией;
-    """
+    def __init__(self, base_url: str):
+        self.url: str = base_url
+        self.context: dict = {}
+        self.bulish_trend: list = []
+        self.bears_trend: list = []
 
-    data_dict: Dict[str, str] = {}
-    indicator: Dict[str, str] = {}
-    avg: Dict[str, str] = {}
-    cnt = 0
-    for statistic in data:
-        _res: str = json.dumps(statistic.text.strip(), ensure_ascii=False)
-        res: str = json.loads(_res)
+    def get_analysis(self) -> Dict[str, str]:
+        """
+        Парсит страницу "investing.com" и собирает данные по отдельным валютным парам.
 
-        key = res.split(":")[0]
-        value = res.split(":")[1]
-        if cnt <= 3:
-            indicator[key] = value
-            cnt += 1
-        else:
-            avg[key] = value
+        :param currency_pair: str - url c валютной парой;
+        :return: Dict[str, str] - словарь с детальной информацией по валютной паре;
+        """
+        try:
+            page: Response = requests.get(self.url)
+            soup: BeautifulSoup = BeautifulSoup(page.text, "html.parser")
 
-    # noinspection PyTypeChecker
-    data_dict.update(indicator=indicator, avg=avg)  # type: ignore
+            title_pair = soup.find("a", attrs={"id": "quoteLink"})
+            data_now = datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")
+            data_vol: ResultSet = soup.find_all(
+                "span", attrs={"class": "uppercaseText"}
+            )
+            indicators: ResultSet = soup.find_all("p", attrs={"class": "inlineblock"})
+            self.context.update(title=title_pair["title"], date_time=data_now)  # type: ignore
 
-    return data_dict
+            for name in data_vol:
+                self.context.update(resume=name.text)
 
+            self.context.update(results=self.details(indicators))
+        except OSError as err:
+            self.context.update(
+                error=type(err),
+                error_message=str(err),
+            )
 
-def save_file(context: dict):
-    try:
-        title = context["title"].replace("/", "_")
-        file_name = f"{title.split(' ')[0]}_Data.json"
-        path_to_file = "data_json/" + file_name
-        print(f"{title} \t: - Считан!")
-        text = json.dumps(context, ensure_ascii=False)
+        self.save_file()
+        return self.context
 
-        with open(path_to_file, "w", encoding="utf8") as file:
-            file.write(f"{text}\n")
-    except KeyError as err:
-        print("Описание Ошибки: - ", err)
+    def details(self, data: ResultSet) -> Dict[str, str]:
+        """
+        Собираем детальную информацию по уровням сигналов индикаторов
+        и возвращаем данные в виде словаря.
+
+        :param data: ResultSet - данные индикаторов;
+        :return: Dict[str, str] - упакованный словарь с детальной информацией;
+        """
+
+        data_dict: Dict[str, str] = {}
+        indicator: Dict[str, str] = {}
+        avg: Dict[str, str] = {}
+        cnt = 0
+
+        for statistic in data:
+            _res: str = json.dumps(statistic.text.strip(), ensure_ascii=False)
+            res: str = json.loads(_res)
+
+            key = res.split(":")[0]
+            value = res.split(":")[1]
+
+            if key.lower() == "покупать":
+                self.bulish_trend.append(int(value))
+            if key.lower() == "продавать":
+                self.bears_trend.append(int(value))
+
+            if cnt <= 3:
+                indicator[key] = value
+                cnt += 1
+            else:
+                avg[key] = value
+
+        bulish = round((sum(self.bulish_trend) * 100) / 24, 2)
+        bears = round((sum(self.bears_trend) * 100) / 24, 2)
+
+        print(f"Buy Trend = {bulish}%")
+        print(f"Sell Trend = {bears}%")
+        # noinspection PyTypeChecker
+        data_dict.update(
+            indicator=indicator,
+            avg=avg,
+            bulish_trend=bulish,
+            bears_trend=bears,
+        )  # type: ignore
+
+        return data_dict
+
+    def save_file(self):
+        try:
+            title = self.context["title"].replace("/", "_")
+            file_name = f"{title.split(' ')[0]}_Data.json"
+            path_to_file = os.path.join(DATA_DIR, file_name)
+            print(self.context["date_time"])
+            print(f"{title} \t: - Считан!")
+            text = json.dumps(self.context, ensure_ascii=False)
+
+            with open(path_to_file, "w", encoding="utf8") as file:
+                file.write(f"{text}\n")
+        except KeyError as err:
+            print("Описание Ошибки: - ", err)
 
 
 if __name__ == "__main__":
     # from Python.Experts.Snake.config.currency_pair import curr_pairs
     curr_pairs = [
         "https://ru.investing.com/technical/technical-analysis",  # EUR/USD
-        "https://ru.investing.com/technical/gbp-usd-technical-analysis",  # GBP/USD
-        "https://ru.investing.com/technical/usd-jpy-technical-analysis",  # USD/JPY
+        # "https://ru.investing.com/technical/gbp-usd-technical-analysis",  # GBP/USD
+        # "https://ru.investing.com/technical/usd-jpy-technical-analysis",  # USD/JPY
     ]
 
-    for pair in curr_pairs:
-        get_analysis(pair)
-        sleep(2)
+    while True:
+        for pair in curr_pairs:
+            trend = PowerTrend(pair)
+            trend.get_analysis()
+            sleep(2)
+        sleep(300)
